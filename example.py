8ed81d22b2b2b60ef0994dacfd26925de7322652
Samuele Santi <redshadow@hackzine.org>
2014-04-29 01:04:42 +0200

    Example script able to parse a pcapng file

---
+++ example.py
@@ -0,0 +1,21 @@
+import logging
+import sys
+
+from pcapng import PCAPNG_Reader
+
+
+logger = logging.getLogger('pcapng')
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stderr)
+formatter = logging.Formatter(
+    '\033[1;37;40m  %(levelname)s  \033[0m \033[0;32m%(message)s\033[0m')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+if __name__ == '__main__':
+    import sys
+    rdr = PCAPNG_Reader(sys.stdin)
+    while True:
+        packet = rdr.read_block()
+        print(repr(packet))
