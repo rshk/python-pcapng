b8f3d7218213004eed4905fafda8d51b302398ce
Julien Iguchi-Cartigny <kartoch@gmail.com>
2014-12-18 16:06:12 +0100

    Add test from wireshark website
    - add traces from <http://wiki.wireshark.org/Development/PcapNg>
    - test them by iterate over elements

---
+++ tests/test_sample_wireshark.py
@@ -0,0 +1,60 @@
+import pcapng
+
+def test_sample_test001_ntar():
+	fp = open('test_data/test001.ntar')
+	reader = pcapng.PcapngReader(fp)
+	for entry in reader:
+		pass
+
+def test_sample_test002_ntar():
+	fp = open('test_data/test002.ntar')
+	reader = pcapng.PcapngReader(fp)
+	for entry in reader:
+		pass
+
+def test_sample_test003_ntar():
+	fp = open('test_data/test003.ntar')
+	reader = pcapng.PcapngReader(fp)
+	for entry in reader:
+		pass
+
+def test_sample_test004_ntar():
+	fp = open('test_data/test004.ntar')
+	reader = pcapng.PcapngReader(fp)
+	for entry in reader:
+		pass
+
+def test_sample_test005_ntar():
+	fp = open('test_data/test005.ntar')
+	reader = pcapng.PcapngReader(fp)
+	for entry in reader:
+		pass
+
+def test_sample_test006_ntar():
+	fp = open('test_data/test006.ntar')
+	reader = pcapng.PcapngReader(fp)
+	for entry in reader:
+		pass
+
+def test_sample_test007_ntar():
+	fp = open('test_data/test007.ntar')
+	reader = pcapng.PcapngReader(fp)
+	for entry in reader:
+		pass
+
+def test_sample_test008_ntar():
+	fp = open('test_data/test008.ntar')
+	reader = pcapng.PcapngReader(fp)
+	for entry in reader:
+		pass
+def test_sample_test009_ntar():
+	fp = open('test_data/test009.ntar')
+	reader = pcapng.PcapngReader(fp)
+	for entry in reader:
+		pass
+
+def test_sample_test010_ntar():
+	fp = open('test_data/test010.ntar')
+	reader = pcapng.PcapngReader(fp)
+	for entry in reader:
+		pass
