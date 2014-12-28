4eec17437661ce192c13e7714d4b40b0e27ae201
Samuele Santi <redshadow@hackzine.org>
2014-12-28 22:38:58 +0100

    New example script, dumping information about a pcapng file

---
+++ examples/dump_pcapng_info.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import pcapng
+import sys
+
+
+def dump_information(scanner):
+    for block in scanner:
+        print(block)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        with open(sys.argv[1], 'rb') as fp:
+            scanner = pcapng.FileScanner(fp)
+            dump_information(scanner)
+
+    else:
+        scanner = pcapng.FileScanner(sys.stdin)
+        dump_information(scanner)
