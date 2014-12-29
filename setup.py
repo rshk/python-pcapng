5be96dafc12b32670f721b0e20394e3fafef318d
Samuele Santi <redshadow@hackzine.org>
2014-12-29 01:57:53 +0100

    Moving development status to "Alpha"

--- setup.py
+++ setup.py
@@ -26,9 +26,9 @@ setup(
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
 
-        "Development Status :: 1 - Planning",
+        # "Development Status :: 1 - Planning",
         # "Development Status :: 2 - Pre-Alpha",
-        # "Development Status :: 3 - Alpha",
+        "Development Status :: 3 - Alpha",
         # "Development Status :: 4 - Beta",
         # "Development Status :: 5 - Production/Stable",
         # "Development Status :: 6 - Mature",
