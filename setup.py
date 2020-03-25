5b37763f2b9b0500f4973ce6ac3fc6d5c67edaa2
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-03-25 20:39:04 +0000

    Replace requirements-dev.txt with a `dev` extra in setup.py.
    This allows now setting up the development environment with a simpler:
    
        pip install -e .[dev]
    
    to include an editable version of the package.

--- setup.py
+++ setup.py
@@ -5,6 +5,17 @@ version = '1.0'
 setup(
     version=version,
     packages=find_packages(),
-    install_requires=['six'],
-    package_data={'': ['README.rst', 'CHANGELOG.rst', 'LICENSE']},
-    zip_safe=False)
+    install_requires=["six"],
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-cov",
+            "pytest-pep8",
+            "flake8",
+            "sphinx",
+            "sphinx-rtd-theme",
+        ],
+    },
+    package_data={"": ["README.rst", "CHANGELOG.rst", "LICENSE"]},
+    zip_safe=False,
+)
