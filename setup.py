9f6a1f70059628f29144102ab451b7b90569cb7a
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-07-16 18:51:53 +0100

    Move all the remaining packaging configuration to setup.cfg.
    Note that the package_data entries were not actually in use: those files
    are top-level rather than part of a package, so the resulting distribution
    is identical.

--- setup.py
+++ setup.py
@@ -1,22 +1,5 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 import setuptools_scm  # noqa: F401
 
-setup(
-    packages=find_packages(),
-    extras_require={
-        "dev": [
-            "isort",
-            "pytest>=5.4",
-            "pytest-cov",
-            "pytest-pycodestyle",
-            "flake8",
-            "pre-commit",
-            "setuptools_scm[toml]",
-            "sphinx",
-            "sphinx-rtd-theme",
-        ],
-    },
-    package_data={"": ["README.rst", "CHANGELOG.rst", "LICENSE"]},
-    zip_safe=False,
-)
+setup()
